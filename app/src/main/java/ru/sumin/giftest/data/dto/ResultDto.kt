ab574b6b01b46a4621d9687b64f5a38adf1f8fc1
a.sumin <a.sumin@cardsmobile.ru>
2020-08-29 17:20:25 +0300

    Небольшой рефакторинг

--- app/src/main/java/ru/sumin/giftest/data/dto/ResultDto.kt
+++ app/src/main/java/ru/sumin/giftest/data/dto/ResultDto.kt
@@ -7,9 +7,9 @@ import com.google.gson.annotations.SerializedName
 class ResultDto {
     @SerializedName("result")
     @Expose
-    private var result: List<ContentDto?>? = null
+    val result: List<ContentDto?>? = null
 
     @SerializedName("totalCount")
     @Expose
-    private var totalCount = 0
+    val totalCount = 0
 }
